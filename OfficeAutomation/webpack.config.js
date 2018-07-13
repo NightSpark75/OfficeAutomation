102069dcf8695877f2e7cc6e1f3581a7d1d7e4c1
spark <Lin.yupin@stantard.com.tw>
2018-07-13 11:41:26 +0800

    complete add ReactJS

---
+++ OfficeAutomation/webpack.config.js
@@ -0,0 +1,34 @@
+﻿const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: { 'main': './js/app.jsx' },
+  output: {
+    path: path.resolve(__dirname, './Scripts/dist'),
+    filename: 'bundle.js',
+    publicPath: './Scripts/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-env'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }
+    ]
+  }
+};
---
+++ OfficeAutomation/webpack.prod.js
@@ -0,0 +1,50 @@
+﻿const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: { 'main': './js/app.js' },
+  output: {
+    path: path.resolve(__dirname, './Script/dist'),
+    filename: 'bundle.js',
+    publicPath: './Script/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-env'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: "commons",
+          chunks: "all",
+          minChunks: 2
+        }
+      }
+    }
+  },
+};
